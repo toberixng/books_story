c765c086a5749bb81e6f588dff0a1c1492254d71
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2020-07-10 23:02:04 +0100

    connection update

--- app.js
+++ app.js
@@ -16,4 +16,4 @@ if(process.env.NODE_ENV === 'development'){
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} node on port ${PORT}`)) 
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} node on port ${PORT}`)) // NEW UPDATE 
--- config/db.js
+++ config/db.js
@@ -1,17 +1,18 @@
+
 const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
-            useUnifiedTopology: true,
+            useUnifiedTopology: true, 
             useFindAndModify: false
         })
 
         console.log(`MongoDB Connected: ${conn.connection.host}`)
     } catch (err) {
         console.error(err)
-        process.exit(1)
+        process.exit(1) // this exit with failure
         
     }
 }
